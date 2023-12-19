9230bd6ada3c4f8fc0e09a6ebb15fab2429fca65
Ranjan Kumar <vtu16964@veltech.edu.in>
2023-12-19 07:50:13 +0530

    evenOdd

---
+++ add.js
@@ -0,0 +1,5 @@
+let a =5;
+let b= 3;
+
+let result = a+b;
+console.log("The sum of two number is: "+result);
---
+++ evenOdd.js
@@ -0,0 +1,7 @@
+let num = 30;
+if(num%2==0){
+    console.log(`${num} is Even `);
+}
+else{
+    console.log(`${num} is Odd`);
+}
